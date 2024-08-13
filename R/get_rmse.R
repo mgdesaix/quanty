b5b089d6f11f3f7f467072bb3f714ac71c180c09
Matt DeSaix <mgdesaix@gmail.com>
2024-08-13 11:20:44 -0600

    added function descr

--- R/get_rmse.R
+++ R/get_rmse.R
@@ -1,3 +1,15 @@
+#' Get RMSE for two matrices
+#'
+#' @param ref.q [matrix] Numeric values of N x K
+#' @param test.q [matrix] Numeric values of N x K
+#'
+#' @return Numeric value
+#' @export
+#'
+#' @examples
+#' test.1 <- matrix(1, nrow = 2, ncol = 2)
+#' test.2 <- matrix(2, nrow = 2, ncol = 2)
+#' get_rmse(test.1, test.2)
 get_rmse <- function(ref.q, test.q){
   stopifnot("Hey! Matrix dimensions need to match" = dim(ref.q) == dim(test.q))
   q.mat2 <- (test.q - ref.q)^2
